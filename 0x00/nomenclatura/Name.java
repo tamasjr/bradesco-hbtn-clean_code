dde96e0ceacbf4e75dc4c20f0109c2f3342a9982
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-10-15 23:55:10 -0300

    Uso da Nomenclatura

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,27 @@
+public class Name {
+
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user) {
+    }
+
+    public List<Account> accountList;
+
+    public void deleteUser(Name.User user) {
+    }
+
+    public URL url;
+
+    public class TableRepresent {}
+    public void saveCar() {
+    }
+
+    public void saveUser() {
+    }
+    public void saveAccount() {
+    }
+    public void saveContract() {
+    }
+
+    public static final int DAYS_IN_YEAR = 360;
+    public static final int HOURS_IN_WEEK = 168;
+}

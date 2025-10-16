a7740c74f97e03c513ae480f74c20e88d8be7544
TAMAS PETER BATTONYAI JUNIOR <153637136+TAMAS-BATTONYAI_bradesco@users.noreply.github.com>
2025-10-16 00:08:31 -0300

    Funções

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,38 @@
+public class Functions {
+
+    public void saveIncome(Income income) {
+        inRepo.save(income);
+    }
+
+    public void saveExpense(Expense expense) {
+        outRepo.save(expense);
+    }
+
+    @PostMapping("/user")
+    public User saveUser(User user) {
+        return userService.save(user);
+    }
+
+    public void saveUser(User user) {
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void setRole(User user) {
+        user.setRole(user);
+    }
+
+    public Environment getEnvironment() {
+        return environmentService.getEnvironment(getVersion());
+    }
+
+    public Release getRelease() {
+        return releaseService.getRelease(getVersion());
+    }
+
+    private String getVersion() {
+        return versionService.getVersion();
+    }
+}
